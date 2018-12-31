2ed19db052bf48bb53809a64ddda1bc87100f221
Nicolas Hermet <n.hermet@gmail.com>
2018-12-31 14:31:23 +0100

    add post array with Hipsum contents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Activite1-Angular';
+  title = "Activité d'Angular pour OpenClassrooms";
+
+  posts = [
+  	{
+  		title: 'PBR&B subway',
+		content: 'PBR&B subway tile celiac deep v, tofu raw denim glossier. Green juice air plant cold-pressed, pabst locavore subway tile asymmetrical blue bottle kitsch taxidermy poutine hashtag flannel. Shoreditch cardigan +1 normcore kombucha adaptogen. Microdosing cold-pressed iPhone 8-bit, williamsburg pitchfork jean shorts copper mug VHS.',
+		loveIts: 0,
+		created_at: Date
+  	},
+  	{
+  		title: 'Ramps cronut ',
+		content: 'Ramps cronut cray hashtag, trust fund butcher salvia authentic tousled tilde literally stumptown. Knausgaard twee direct trade, paleo chartreuse gastropub activated charcoal succulents letterpress tote bag readymade coloring book chia etsy waistcoat. Taiyaki artisan small batch selvage lomo distillery literally sriracha. Swag pour-over dreamcatcher chillwave lo-fi jianbing leggings, VHS shoreditch raw denim art party. Meh air plant flannel swag letterpress seitan hella kale chips offal. Prism listicle tilde, gluten-free humblebrag green juice roof party PBR&B mixtape.',
+		loveIts: 0,
+		created_at: Date
+  	},
+  	{
+  		title: 'Master cleanse',
+		content: "Master cleanse next level skateboard, jianbing chia chambray venmo chillwave distillery meh helvetica squid affogato man bun bespoke. Lomo paleo lyft echo park, tousled iceland prism narwhal distillery literally woke knausgaard intelligentsia YOLO you probably haven't heard of them. Affogato everyday carry plaid poutine dreamcatcher, man bun letterpress. Pour-over semiotics kitsch chambray, vape gluten-free try-hard bitters poutine lo-fi thundercats. Vaporware hella selvage air plant migas.",
+		loveIts: 0,
+		created_at: Date
+  	}
+  ];
 }
