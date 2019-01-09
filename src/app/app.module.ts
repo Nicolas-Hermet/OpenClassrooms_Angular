fb9a1773ce9fac21ae67a71d4d83eaf9b9a19d99
Nicolas Hermet <n.hermet@gmail.com>
2019-01-09 14:33:04 +0100

    add postListItem component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostlistComponent } from './postlist/postlist.component';
 import { FormsModule } from '@angular/forms';
+import { PostlistitemComponent } from './postlistitem/postlistitem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostlistComponent
+    PostlistComponent,
+    PostlistitemComponent
   ],
   imports: [
     BrowserModule,
